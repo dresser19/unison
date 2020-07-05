766f9b68c2823033018ac158bd79d572d8a7d780
dresser19 <63084350+dresser19@users.noreply.github.com>
2020-07-05 17:01:04 +0900

    Update audioPlayer.js

--- audioPlayer.js
+++ audioPlayer.js
@@ -22,8 +22,37 @@ function audioPlayer(){
       });
   }
 
-  function displayLyrics() {
-      $('.lyricsBox > pre').hide();
-      $(".lyricsBox > pre[data-file='"+ $("#audioPlayer").attr("src").substr(-7) +"']").show();
-      $('.lyricsBox').show();
-  }
+
+  audioPlayer = function() {
+        var currentSong = 0;
+        $("#audioPlayer")[0].src = $("#playlist li a")[0];
+        $("#audioPlayer")[0].play();
+        $("#playlist li a").click(function(e){
+           e.preventDefault();
+           $("#audioPlayer")[0].src = this;
+           $("#audioPlayer")[0].play();
+           $("#playlist li").removeClass("current-song");
+            currentSong = $(this).parent().index();
+            $(this).parent().addClass("current-song");
+            displayLyrics(false);
+        });
+
+        $("#audioPlayer")[0].addEventListener("ended", function(){
+           currentSong++;
+            if(currentSong == $("#playlist li a").length)
+                currentSong = 0;
+            $("#playlist li").removeClass("current-song");
+            $("#playlist li:eq("+currentSong+")").addClass("current-song");
+            $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+            $("#audioPlayer")[0].play();
+            displayLyrics(false);
+        });
+    }
+
+    displayLyrics = function(clicked) {
+        $('.lyricsBox > pre').hide();
+        $(".lyricsBox > pre[data-file='"+ $("#audioPlayer").attr("src").substr(-7) +"']").show();
+        if(clicked) {
+            $('.lyricsBox').show();
+        }
+      }
